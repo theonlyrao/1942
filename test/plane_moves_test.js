2086106efed27d3b84420894f0556cfa939aae03
thomp <thompickett@gmail.com>
2016-07-06 19:49:31 -0600

    Refactor into separate files and classes
    Extract plane into a separate plane object
    Extract rendering duties a render object
    `script.js` only runs thing and does some 
     event listeners
    Remove script from html file so that it only 
    inherits from main.bundler.js

---
+++ test/plane_moves_test.js
@@ -0,0 +1,93 @@
+const assert = require('chai').assert;
+
+const Plane = require('../lib/javascripts/plane');
+
+describe('Plane', function() {
+  context('is default', function() {
+    var plane = new Plane({type: "player"});
+    var attributes = { type: "player", x: 350, y: 410,
+                       width: 8, height: 25, wingspan: 52,
+                       tailspan: 16}
+
+    it('default planes type is player', function() {
+      assert.equal(plane.type, attributes.type);
+    });
+
+    it('player planes x coordinate', function() {
+      assert.equal(plane.x, attributes.x);
+    });
+
+    it('player planes y coordinate', function() {
+      assert.equal(plane.y, attributes.y);
+    });
+
+    it('player planes width', function() {
+      assert.equal(plane.width, attributes.width);
+    });
+
+    it('player planes height', function() {
+      assert.equal(plane.height, attributes.height);
+    });
+
+    it('player planes wingspan', function() {
+      assert.equal(plane.wingspan, attributes.wingspan);
+    });
+
+    it('player planes tailspan', function() {
+      assert.equal(plane.tailspan, attributes.tailspan);
+    });
+
+  });
+
+  context('is told to move', function() {
+    it('plane is told to move north', function() {
+      var plane = new Plane({x: 350,y: 410});
+      plane.moveNorth()
+      assert.equal(plane.y, 403);
+    });
+
+    it('plane is told to move south', function() {
+      var plane = new Plane({x: 350,y: 410});
+      plane.moveSouth()
+      assert.equal(plane.y, 417);
+    });
+
+    it('plane is told to move east', function() {
+      var plane = new Plane({x: 350,y: 410});
+      plane.moveEast()
+      assert.equal(plane.x, 357);
+    });
+
+    it('plane is told to move west', function() {
+      var plane = new Plane({x: 350,y: 410});
+      plane.moveWest()
+      assert.equal(plane.x, 343);
+    });
+
+    it('plane doesnt move north if it hits boundary', function() {
+      var plane = new Plane({x: 350,y: 26});
+      plane.moveNorth()
+      assert.equal(plane.y, 26);
+    });
+
+    it('plane doesnt move south if it hits boundary', function() {
+      var plane = new Plane({x: 350,y: 454});
+      plane.moveSouth()
+      assert.equal(plane.y, 454);
+    });
+
+    it('plane transfers to west it hits boundary east boundary', function() {
+      var plane = new Plane({x: 731,y: 350});
+      plane.moveEast()
+      assert.equal(plane.x, -30);
+    });
+
+    it('plane transfers to west it hits boundary east boundary', function() {
+      var plane = new Plane({x: -29,y: 350});
+      plane.moveWest()
+      assert.equal(plane.x, 730);
+    });
+
+  });
+
+});
