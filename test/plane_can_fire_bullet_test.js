fdf9553e4c8043bbacca8d5d313b9d00f2e43e15
Ashwin Rao <theonlyrao@gmail.com>
2016-07-08 23:47:57 -0600

    Allow computer plane to destroy player
    The ability of a computer plane to fire is now tested. Collision
    detection is also called to see if a computer plane has hit a player plane.

--- test/plane_can_fire_bullet_test.js
+++ test/plane_can_fire_bullet_test.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 const Plane = require('../lib/javascripts/plane');
 
 describe('Plane', function() {
-    context('at new location', function() {
+    context('player at new location', function() {
 	let plane = new Plane({x: 190, y: 200, type: "player" });
 
 	it('can fire bullet', function() {
@@ -14,4 +14,17 @@ describe('Plane', function() {
 	    assert.equal(plane.bullets.length, 1)
 	});
     });
+
+    context('computer at new location', function() {
+	let plane = new Plane({x: 190, y: 200, type: "computer" });
+
+	it('can fire bullet', function() {
+	    let playerBullet = plane.fire();
+	    assert.equal(playerBullet.x, plane.x + plane.width/2);
+	    assert.equal(playerBullet.y, plane.y + 3);
+	    assert.equal(playerBullet.type, plane.type)
+	    assert.equal(plane.bullets.length, 1)
+	});
+    });
+
 });
