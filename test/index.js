d24b729389dc3e87be2d7d85d1e57f6508279a0a
Ashwin Rao <theonlyrao@gmail.com>
2016-07-08 18:31:01 -0600

    Build collision detection
    Collision detector works now using the fuselage of two planes by
    evaluating if any of the points in the boudaries are in common.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,3 @@
-//require('./plane_moves_test.js');
-//require('./destroy_planes_test.js');
+require('./plane_moves_test.js');
+require('./destroy_planes_test.js');
 require('./fuselage_has_a_boundary_test.js');
